13cab6def8af75ea16a765d7194a1d49a1543577
unknown <asifrizwan6989@gmail.com>
2020-04-16 19:09:53 +0530

    Some required feild are changed

--- models/content.model.js
+++ models/content.model.js
@@ -5,11 +5,11 @@ const contentSchema = new Schema({
     description: {type: String, required: true}, 
     duration: {type: Number, required:true},
     date:{type:Date, required:true},
-    cast:{type: String, required: true},
-    producedBy:{type: String, required: true},
-    director:{type: String, required: true},
-    music:{type: String, required: true},
-    language:{type: String, required: true}
+    cast:{type: String},
+    producedBy:{type: String},
+    director:{type: String},
+    music:{type: String},
+    language:{type: String}
 },{
 timestamps: true,
 });
